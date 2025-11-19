b2e3a5920affdfd229e94520d394fbb2db17bf62
Thota <srikanth.thota@siemens.com>
2025-11-19 21:47:17 +0530

    Added one more function

--- main.py
+++ main.py
@@ -47,3 +47,10 @@ async def get_euron_data():
     return iterms
 
 
+@app.get("/euron/showdata")
+async def show_euron_data():
+    iterms = []
+    cursor = euron_data.find({})
+    async for document in cursor:
+        iterms.append(euron_helper(document))
+    return iterms
